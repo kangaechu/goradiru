9b4c6ff0cbc12432f1569561a4ef87d6f56d994f
Kangaechu <kangae2@gmail.com>
2019-06-05 20:52:06 +0900

    move load config into config.go

--- config.go
+++ config.go
@@ -1,5 +1,13 @@
 package goradiru
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"path/filepath"
+)
+
 type config struct {
 	ProgDir                   string `mapstructure:"ProgDir"`
 	FileType                  string `mapstructure:"FileType"`
@@ -14,4 +22,29 @@ var sharedInstance = &config{ /* 初期化 */ }
 
 func GetConfig() *config {
 	return sharedInstance
+}
+
+func LoadConfig() *config {
+	// スクリプトのディレクトリを取得
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	viper.SetConfigName("conf")                       // 設定ファイルのファイル名
+	viper.AddConfigPath(filepath.Join(dir, "config")) // 設定ファイルのディレクトリ名
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("error on reading config: %s \n", err))
+	}
+	config := GetConfig()
+
+	err = viper.Unmarshal(config)
+	if err != nil {
+		panic(fmt.Errorf("error on parsing config %s \n", err))
+	}
+	config.ProgDir = filepath.Join(dir, config.ProgDir)
+	config.DownloadedHistoryConfFile = filepath.Join(dir, config.DownloadedHistoryConfFile)
+	return config
+
 }
